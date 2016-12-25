b142a3df912de39251cce25e5fd9f72764c912eb
Srini <ssomayajula@saisruthis-imac.home>
2016-12-25 10:51:05 -0500

    Initial Commit

---
+++ Test_Hello_World/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test_Hello_World
+//
+//  Created by Srini on 12/25/16.
+//  Copyright © 2016 Srini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
